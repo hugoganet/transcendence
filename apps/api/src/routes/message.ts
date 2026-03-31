accbe2d9f7045f02997eeccf9b7762c2618a2ca7
theo <theo-vester@hotmail.fr>
2026-03-31 14:17:00 +0200

    added io socket right after the send message so we emit the message to the receiver room id

--- apps/api/src/routes/message.ts
+++ apps/api/src/routes/message.ts
@@ -1,6 +1,7 @@
 import { Router, type Request, type Response } from "express";
 import { requireAuth } from "../middleware/auth.js";
 import { sendMessage, getConversation } from "../services/messageService.js";
+import { getIO } from "../socket/index.js";
 
 export const messageRouter = Router()
 
@@ -8,6 +9,7 @@ messageRouter.post("/", requireAuth, async (req: Request, res: Response) => {
     const user = req.user as Express.User;
     const { receiverId, content } = req.body;
     const data = await sendMessage(user.id, receiverId, content);
+    getIO().to(`user:${receiverId}`).emit("message:new", data);
     res.status(201).json({ data });
 });
 
